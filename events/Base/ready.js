a6e4d28d0024c809459c4aa32dcdf6b8949770f1
Azrog <78275913+azrogdev@users.noreply.github.com>
2022-10-30 15:50:50 +0100

    Update ready.js

--- events/Base/ready.js
+++ events/Base/ready.js
@@ -1,25 +1,23 @@
 require("dotenv").config()
 const { REST } = require("@discordjs/rest")
 const { Routes } = require("discord-api-types/v9")
-const { Client, Intents, Collection } = require("discord.js")
-const { slashCommands } = require("../../util/loader")
+const { slashCommands } = require("../../functions/loader")
 module.exports = async (client) => {
-
     const rest = new REST({
         version: "9"
     }).setToken(process.env.TOKEN);
 
     (async () => {
         try {
-            
-                await rest.put(Routes.applicationGuildCommands("995327553143312474", "977507903307145216"), {
-                    body: slashCommands
-                })
-                console.log(`Les commandes sont prêtes à être utilisées`)
 
-        } catch(err) {
-            if(err) console.error(err)
+            await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+                body: slashCommands
+            })
+            console.log(`(/) commands are operational`)
+
+        } catch (err) {
+            if (err) console.error(err)
         }
     })()
-    client.channels.cache.get("979097878540726303").send({content: "test"})
+
 }
