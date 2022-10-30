46e0ed03db3a71adc57d583af2302142ffd04fa1
Azrog <78275913+azrogdev@users.noreply.github.com>
2022-10-30 16:27:44 +0100

    Create giveaway-end.js

---
+++ commands/Base/giveaway-end.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { MessageEmbed } = require("discord.js")
+
+
+
+module.exports = {
+    help: {
+        name: "giveaway-end"
+    },
+    data: new SlashCommandBuilder()
+        .setName("giveaway-end")
+        .setDescription("Finir un giveaway")
+        .addStringOption(option => option.setName('id').setDescription('\'identifiant du giveaway').setRequired(true)),
+
+
+    async execute(interaction) {
+      
+        const message = await interaction.channel.messages.fetch(interaction.options.getString('id'))
+        const reaction = await giveaway.reactions.cache.get("🎉").fetch()
+        const winners = 1
+                                    const users = await reaction.users.fetch()
+                                    const winnerList = [];
+                                    const participants = [];
+                                    users.forEach(( user ) => {
+                                        if(!user.bot) participants.push(user)
+                                    });
+                                        
+                                        for(let i = 0; i < winners; i++) {
+                                            const random = Math.floor(Math.random() * participants.length);
+                                            const winner = participants[random];
+                                            const winIndex = participants.indexOf(winner);
+                                            participants.splice(winIndex, 1);
+                                            winnerList.push(winner);
+                                        }
+                                    
+                                        interaction.reply({ content: `Bien joué au gagnant du giveaway qui est : ${winnerList.join(", ")}`});
+                                    
+    }
+}
