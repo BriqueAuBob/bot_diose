d9b8bb62beba278654571cf3b442b588cdc5cb8c
BriqueAuBob <briqueaubob@gmail.com>
2022-12-04 00:16:53 +0100

    update

--- events/Guild/guildMemberAdd.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (client, member) => {
-    if(member.guild.id !== "977507903307145216") return
-    member.roles.add("985505448398577674")
-    client.channels.cache.get("977511559234486352").send({content: `<:flower:997180854046556320> Arrivée de ${member} tout en douceur sur **UMaestro** !`})
-}
